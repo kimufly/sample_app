c8c368b314cc585e78d8326a3f6bf91347e5671b
Kimmg <kimufly@live.com>
2015-11-13 12:03:35 +0800

    try erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  root 'static_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
