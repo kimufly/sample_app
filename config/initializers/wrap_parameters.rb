2b08e6ad43b8c6076be54ab9e81fa109260deabd
Kimmg <kimufly@live.com>
2015-11-15 22:41:28 +0800

    application export

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -1,14 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
-# This file contains settings for ActionController::ParamsWrapper which
+# This file contains settings for ActionControllerParamsWrapper which
 # is enabled by default.
 
-# Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
-ActiveSupport.on_load(:action_controller) do
-  wrap_parameters format: [:json] if respond_to?(:wrap_parameters)
+# Enable parameter wrapping for JSON. You can disable this by setting format to an empty array.
+ActiveSupport.on_load(action_controller) do
+  wrap_parameters format [json] if respond_to?(wrap_parameters)
 end
 
 # To enable root element in JSON for ActiveRecord objects.
-# ActiveSupport.on_load(:active_record) do
+# ActiveSupport.on_load(active_record) do
 #  self.include_root_in_json = true
 # end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,31 +1,31 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
+class StaticPagesControllerTest < ActionControllerTestCase
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get home" do
-    get :home
-    assert_response :success
+    get home
+    assert_response success
     assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
-    get :help
-    assert_response :success
+    get help
+    assert_response success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get :about
-    assert_response :success
+    get about
+    assert_response success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get :contact
-    assert_response :success
+    get contact
+    assert_response success
     assert_select "title", "Contact | #{@base_title}"
   end
 
